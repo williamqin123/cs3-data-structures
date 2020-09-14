44028fb38089e054ded8a85004f6b7da3e9fcc85
SLHSCS210 <SLHSCS210@katyisd.org>
2020-09-14 13:18:47 -0500

    Histogram done

--- eclipse-workspace/Histogram/src/Histogram.java
+++ eclipse-workspace/Histogram/src/Histogram.java
@@ -1,4 +1,27 @@
 
 public class Histogram {
+	
+	int[] histogram;
+	
+	public Histogram(String digitStr) {
+		histogram = new int[10];
+		fillArray(digitStr);
+	}
+	
+	public void fillArray(String digitStr) {
+		for (char c : digitStr.toCharArray()) {
+			histogram[Integer.parseInt(String.valueOf(c))]++;
+		}
+	}
 
+	@Override
+	public String toString() {
+		String output = "";
+		
+		for (int digit = 0; digit < 10; digit++) {
+			output += digit + " - " + histogram[digit] + "\n";
+		}
+		
+		return output.substring(0, output.length() - 1); // cuts off the ending newline
+	}
 }
--- eclipse-workspace/Histogram/src/HistogramRunner.java
+++ eclipse-workspace/Histogram/src/HistogramRunner.java
@@ -1,9 +1,21 @@
 
+import java.io.*;
+import java.util.*;
+
 public class HistogramRunner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scanner = new Scanner(new File("histogram.txt"));
+		
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			
+			Histogram lineHistogram = new Histogram(line);
+			
+			System.out.println(lineHistogram.toString());
+			
+			System.out.println(); // space between histograms
+		}
 	}
-
 }
