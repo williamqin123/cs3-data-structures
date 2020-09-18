f40796a7aaac8188ac5633a3ff316058c4770620
William <williamqin@Williams-MacBook-Pro.local>
2020-09-18 02:38:39 -0500

    Dominoes bro 2

--- eclipse-workspace/Straight Dominoes/src/Animation.java
+++ eclipse-workspace/Straight Dominoes/src/Animation.java
@@ -1,4 +1,18 @@
 
 public class Animation {
 
+	public static double linterp(double y0, double y1, double x) {
+		if (x < y0) return 0;
+		else if (x > y1) return 1;
+		
+		return x * y1 + (1.0 - x) * y0;
+	}
+	
+	public static double smootherstep(double y0, double y1, double x) {
+		if (x < y0) return 0;
+		else if (x > y1) return 1;
+		
+		return (Math.pow(x, 3)) * (x * (x * 6 - 15) + 10) * (y1 - y0) + y0;
+	}
+	
 }
