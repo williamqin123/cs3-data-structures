053ec0c4a59100bdb04a0cedfe01e2c4a088ef12
SLHSCS210 <SLHSCS210@katyisd.org>
2020-10-06 13:42:29 -0500

    hard class

--- eclipse-workspace/Lab 4 - Remove Cards/src/RemoveCards.java
+++ eclipse-workspace/Lab 4 - Remove Cards/src/RemoveCards.java
@@ -1,6 +1,8 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
@@ -11,23 +13,77 @@ public class RemoveCards {
 	}
 	
 	public static ArrayList<UnoCards> listRangeToRemove(String start, String end, UnoDeck deck) {
-    }
-	
-	public static void listValueToRemove(String val, UnoDeck deck) {
+		
+		ArrayList<UnoCards> cardsToRemove = new ArrayList<UnoCards>();
 		
 		ListIterator<UnoCards> iter = deck.getDeck().listIterator();
 		
 		while (iter.hasNext()) {
-			if (iter.next().getValue().equals(val)) {
-				iter.remove();
+			UnoCards i = iter.next();
+			int io = Arrays.asList(UnoDeck.values).indexOf(i.getValue());
+			if (io >= Arrays.asList(UnoDeck.values).indexOf(start) && io <= Arrays.asList(UnoDeck.values).indexOf(end)) {
+				cardsToRemove.add(i);
 			}
 		}
+		
+		Collections.sort(cardsToRemove, new Comparator() {
+			public int compare(StarTrek o1, StarTrek o2) {
+		        if (o1.ordinal() < 3)
+		            return o2.ordinal() < 3 ? o1.ordinal() - o2.ordinal() : 1;
+		        return o2.ordinal() < 3 ? -1 : o1.name().compareTo(o2.name());
+		    }
+		});
+		
+		return cardsToRemove;
+    }
+	
+	public static ArrayList<UnoCards> listValueToRemove(String val, UnoDeck deck) {
+		
+		ArrayList<UnoCards> cardsToRemove = new ArrayList<UnoCards>();
+		
+		ListIterator<UnoCards> iter = deck.getDeck().listIterator();
+		
+		while (iter.hasNext()) {
+			UnoCards i = iter.next();
+			if (i.getValue().equals(val)) {
+				cardsToRemove.add(i);
+			}
+		}
+		
+		return cardsToRemove;
     }
 	
 	public static ArrayList<UnoCards> listColorToRemove(String val, UnoDeck deck) {
+		
+		ArrayList<UnoCards> cardsToRemove = new ArrayList<UnoCards>();
+		
+		ListIterator<UnoCards> iter = deck.getDeck().listIterator();
+		
+		while (iter.hasNext()) {
+			UnoCards i = iter.next();
+			if (i.getColor().equals(val)) {
+				cardsToRemove.add(i);
+			}
+		}
+		
+		return cardsToRemove;
     }
 	
 	public static int removal(ArrayList<UnoCards> findList, UnoDeck deck) {
+		
+		ListIterator<UnoCards> iter = deck.getDeck().listIterator();
+		
+		int moves = 0;
+		while (findList.size() > 0) {
+			UnoCards i = iter.next();
+			if (findList.contains(i)) {
+				iter.remove();
+				findList.remove(i);
+			}
+			moves++;
+		}
+		
+		return moves;
 	}
 	
 	//other necessary methods
@@ -69,7 +125,7 @@ public class RemoveCards {
 		String inputDeck = sc.nextLine();
 		UnoDeck deckCards = new UnoDeck(inputDeck);
 		
-		System.out.println(deckCards);
+		System.out.println("Deck: " + deckCards);
 		
 		System.out.println("Please enter command. Enter STOP to end program");
 		
@@ -84,15 +140,36 @@ public class RemoveCards {
 			String command = tokens[0];
 			String[] arguments = Arrays.copyOfRange(tokens, 1, tokens.length);
 			
+			int moves = 0;
+			
 			switch (command) {
-			case "value":
-				listValueToRemove(arguments[0], deckCards);
-				System.out.println(deckCards);
-				break;
-				
-			default: // invalid command
-				System.out.println("Command \"" + command + "\" not found. Try again.");
+				case "value":
+					System.out.println("remove " + arguments[0]);
+					moves = removal(listValueToRemove(arguments[0], deckCards), deckCards);
+					break;
+					
+				case "color":
+					System.out.println("remove " + arguments[0]);
+					moves = removal(listColorToRemove(arguments[0], deckCards), deckCards);
+					break;
+					
+				case "range":
+					System.out.println("remove " + arguments[0]);
+					moves = removal(listRangeToRemove(arguments[0], arguments[1], deckCards), deckCards);
+					break;
+					
+				case "rangecolor":
+					System.out.println("remove " + arguments[0]);
+					//moves = removal(listRangeColorToRemove(arguments[0], deckCards), deckCards);
+					break;		
+					
+				default: // invalid command
+					System.out.println("Command \"" + command + "\" not found. Try again.");
+					continue;
 			}
+			
+			System.out.println("Number of moves for removal: " + moves);
+			System.out.println("Deck: " + deckCards);
 			
 			entry = sc.nextLine();
 		}
