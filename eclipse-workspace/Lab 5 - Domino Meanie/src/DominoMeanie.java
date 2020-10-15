d61f8cef47fc2e963ce7343a931f8b31269bb051
SLHSCS210 <SLHSCS210@katyisd.org>
2020-10-15 13:42:38 -0500

    buh

--- eclipse-workspace/Lab 5 - Domino Meanie/src/DominoMeanie.java
+++ eclipse-workspace/Lab 5 - Domino Meanie/src/DominoMeanie.java
@@ -1,4 +1,11 @@
 
 public class DominoMeanie {
 
+    public static void main(String args[])
+	{
+		//provided code
+		out.println("Enter in the random seed value:");
+		randSeed = input.nextInt();
+		randNum = new Random(randSeed);
+	}
 }
--- eclipse-workspace/Lab 5 - Domino Meanie/src/DominoSet.java
+++ eclipse-workspace/Lab 5 - Domino Meanie/src/DominoSet.java
@@ -1,4 +1,62 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.ListIterator;
+
+// Based on Lab 04 - Line of Dominoes
+
 public class DominoSet {
 
+	private List<Domino> setOfDominoes = new ArrayList<Domino>();
+	
+	public List<Domino> getList() {
+		return setOfDominoes;
+	}
+	
+	public int size() {
+		return getList().size();
+	}
+	
+	public DominoSet(List<Domino> dominoList) {
+		setOfDominoes = dominoList;
+	}
+	
+	public DominoSet(int maxPips) {
+		// avoids using indexes in for loops
+		IntStream.range(0, maxPips + 1).forEachOrdered(p1 -> {
+			IntStream.range(p1, maxPips + 1).forEachOrdered(p2 -> {
+			    Domino d = new Domino(p1, p2);
+			    /* if (Math.random() >= 0.5) {
+			    	d.rotate();
+			    }*/
+			    setOfDominoes.add(d);
+			});
+		});
+	}
+	
+	public void shuffle(int randSeed)
+	{
+		Random rand = new Random(randSeed);
+		for(int i=0; i<setOfDominoes.size(); i++)
+		{
+			if(rand.nextBoolean())
+				setOfDominoes.get(i).rotate();
+			int k = rand.nextInt(setOfDominoes.size());
+			Domino temp = setOfDominoes.get(k);
+			setOfDominoes.set(k,setOfDominoes.get(i));
+			setOfDominoes.set(i, temp);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String dominoSetString = "";
+		ListIterator<Domino> iter = setOfDominoes.listIterator();
+		while (iter.hasNext()) {
+			dominoSetString += iter.next().toString();
+		}
+		return dominoSetString;
+	}
 }
