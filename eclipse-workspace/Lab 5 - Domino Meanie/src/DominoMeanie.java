eea21faf07bbd71bda16eb659ffa208d134696a2
William <williamqin@Williams-MacBook-Pro.local>
2020-10-21 03:40:30 -0500

    meanie

--- eclipse-workspace/Lab 5 - Domino Meanie/src/DominoMeanie.java
+++ eclipse-workspace/Lab 5 - Domino Meanie/src/DominoMeanie.java
@@ -1,11 +1,59 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 public class DominoMeanie {
+	
+	private static List<DominoSet> cyclicDistributeDominoesToPlayers(DominoSet allDominoes, int playerCount) {
+		
+		ListIterator<Domino> iter = allDominoes.getList().listIterator();
+		List<DominoSet> playerHands = new ArrayList<DominoSet>();
+		
+		IntStream.range(0, playerCount).forEachOrdered(p -> {
+			playerHands.add(new DominoSet(new ArrayList<Domino>()));
+		});
+		
+		RepeatingListIterator<DominoSet> playersIter = new RepeatingListIterator<DominoSet>(playerHands);
+		
+		while (iter.hasNext()) {
+			playersIter.next().getList().add(iter.next());
+		}
+		
+		return playerHands;
+	}
+	
+	private static List<DominoSet> splitDominoSetToPlayers(DominoSet allDominoes, int playerCount) {
+		
+		final int numberOfDominoes = allDominoes.getList().size();
+		ListIterator<Domino> iter = allDominoes.getList().listIterator();
+		ArrayList<Domino> toGivePlayer = new ArrayList<Domino>();
+		
+		List<DominoSet> playerHands = new ArrayList<DominoSet>();
+		
+		int div = 1;
+		int given = 0;
+		
+		while (iter.hasNext()) {
+			toGivePlayer.add(iter.next());
+			int currentHandSize = toGivePlayer.size();
+			if (currentHandSize >= numberOfDominoes * div / playerCount - given) {
+				given += currentHandSize;
+				playerHands.add(new DominoSet((List<Domino>)toGivePlayer.clone()));
+				toGivePlayer.clear();
+				div++;
+			}
+		}
+		
+		return playerHands;
+	}
 
     public static void main(String args[]) {
     	
@@ -27,19 +75,44 @@ public class DominoMeanie {
 		
 		
 		DominoSet allDominoes = new DominoSet(maxPips);
-		ListIterator iter = allDominoes.getList().listIterator();
-		List toGivePlayer = new ArrayList<Domino>();
+		allDominoes.shuffle(randSeed);
+		List<DominoSet> playerHands = cyclicDistributeDominoesToPlayers(allDominoes, playerCount);
 		
-		int div = 0;
-		IntStream.range(0, playerCount).forEachOrdered(p -> {
-			
-			if ()
-			
-			div++;
-		});
+		System.out.println(playerHands);
 		
 		
 		
 		input.close();
+		
+		Map<String,Integer> m = new TreeMap<String,Integer>();
+		System.out.println(m.put("8",1));
+		System.out.println(m.put("8",2));
+		m.put("8",3);
+		m.put("5",4);
+		m.put("6",5);
+		m.put("2",6);
+		System.out.println(m);
+	}
+}
+
+class RepeatingListIterator<T> {
+	
+	private List<T> list;
+	private ListIterator<T> iter;
+	
+	public RepeatingListIterator(List<T> l) {
+		list = l;
+		iter = l.listIterator();
+	}
+	
+	public T next() {
+		if (!iter.hasNext())
+			iter = list.listIterator();
+		
+		return (T)iter.next();
+	}
+	
+	public boolean hasNext() {
+		return list.size() > 0;
 	}
 }
