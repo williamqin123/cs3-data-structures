29560e2220dd56fa849b319510a12a5aaacf8110
SLHSCS210 <SLHSCS210@katyisd.org>
2020-10-27 13:41:56 -0500

    g

--- eclipse-workspace/Lab 6 - Part Lists/src/Part.java
+++ eclipse-workspace/Lab 6 - Part Lists/src/Part.java
@@ -1,8 +1,17 @@
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Part {
 
-	public Part() {
-		
+	String partString;
+	
+	public Part(String data) {
+		partString = " ".join(Collections.reverse(Arrays.asList(data.split("[0-9] \\w"))).toArray());
 	}
 	
+	@Override
+	public String toString() {
+		return partString;
+	}
 }
--- eclipse-workspace/Lab 6 - Part Lists/src/PartList.java
+++ eclipse-workspace/Lab 6 - Part Lists/src/PartList.java
@@ -4,17 +4,35 @@ import java.util.TreeMap;
 
 public class PartList {
 	
-	private Map<Part, Integer> inventory = new TreeMap<Part, Integer>();
+	private Map<Part, Integer> inventory;
 	
 	public PartList() {
-		
+		inventory = new TreeMap<Part, Integer>();
 	}
 	
 	public void addPart(Part part) {
-		
+		inventory.putIfAbsent(part, 0);
+		inventory.put(part, inventory.get(part) + 1);
 	}
 	
 	public void addPart(String partStr) {
 		addPart(new Part(partStr));
+	}
+	
+	public String orderSheet(int size) {
+		String returnVal = "";
+		for (Part p : inventory.keySet()) {
+			returnVal += size - inventory.get(p) + " " + p + "\n";
+		}
+		return returnVal;
+	}
+	
+	@Override
+	public String toString() {
+		String returnVal = "";
+		for (Part p : inventory.keySet()) {
+			returnVal += p + " - " + inventory.get(p) + "\n";
+		}
+		return returnVal;
 	}
 }
