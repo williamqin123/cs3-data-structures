c6e2e3999ce2c5e236ecb599f1c860c38d1f732f
SLHSCS210 <SLHSCS210@katyisd.org>
2020-11-10 13:42:15 -0600

    ab

---
+++ eclipse-workspace/Lab 7 - Prefix/src/PreFix.java
@@ -0,0 +1,15 @@
+
+public class PreFix {
+	
+	public static double add(double a, double b) {
+		return a + b;
+	}
+	public static double add(String a, double b) {
+		return opSwitch(a) + b;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- eclipse-workspace/Lab 7 - Stack Class Creation/src/IntStack.java
+++ eclipse-workspace/Lab 7 - Stack Class Creation/src/IntStack.java
@@ -1,13 +1,33 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class IntStack {
+public class IntStack extends ArrayList<Integer> {
 	
-	List<Integer> contents = new ArrayList<Integer>();
+	public void push(int eger) {
+		add(0, eger);
+	}
 
-	public IntStack() {
-		
+	public int peek() {
+		return get(0);
 	}
 	
+	public int pop() {
+		final int returnVal = peek();
+		remove(0);
+		return returnVal;
+	}
+	
+	public String normalToString() {
+		return super.toString();
+	}
+	
+	@Override
+	public String toString() {
+		final List reversed = (List)this.clone();
+		Collections.reverse(reversed);
+		
+		return ((IntStack)reversed).normalToString();
+	}
 }
