c3f21c8c3535a3646b21df524a3b063df58ca87f
William <williamqin@Williams-MacBook-Pro.local>
2020-11-12 04:36:06 -0600

    Prefix done

--- eclipse-workspace/Lab 7 - Prefix/src/PreFix.java
+++ eclipse-workspace/Lab 7 - Prefix/src/PreFix.java
@@ -1,50 +1,60 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PreFix {
 	
-	public static String pop(String s, boolean skip) {
-		String s2 = s;
-		int go = 1;
-		while (go > 0) {
-			s2 = s2.substring(s.indexOf(" ") + 1);
-			go--;
-			if (skip && (s2.charAt(0) == '+' || s2.charAt(0) == '-' || s2.charAt(0) == '*' || s2.charAt(0) == '/'))
-				go = 2;
-		}
-		return s2;
+	public static String pop(String s) {
+		return s.substring(s.indexOf(" ") + 1);
 	}
 	
-	public static double opSwitch(String s) {
-		char token = s.charAt(0);
+	public static boolean isNumber(String s) {
+		return Character.isDigit(s.charAt(0)) || (s.charAt(0) == '-' && Character.isDigit(s.charAt(1)));
+	}
+	
+	public static double evaluate(String ex) {
 		
-		if (token == '+') {
-			return add(pop(s, false));
+		if (isNumber(ex)) return Double.parseDouble(ex.substring(0, ex.indexOf(' ')));
+		
+		String ex2 = pop(ex);	
+		double operand1 = evaluate(ex2);
+		double operand2;
+		if (isNumber(ex2)) operand2 = evaluate(pop(ex2));
+		else {
+			int stackDepth = 1;
+			Stack<Integer> count = new Stack<Integer>();
+			count.add(0);
+			
+			ex2 = pop(ex2);
+			while (stackDepth > 0) {
+				count.set(count.size() - 1, count.peek() + 1);
+				if (!isNumber(ex2)) {
+					stackDepth++;
+					count.add(0);
+				}
+				ex2 = pop(ex2);
+				while (count.size() > 0 && count.peek() >= 2) {
+					stackDepth--;
+					count.pop();
+				}
+			}
+			operand2 = evaluate(ex2);
 		}
-		else if (token == '-') {
-			return subtract(pop(s, false));
+		
+		switch (ex.charAt(0)) {
+			case '+':
+				return operand1 + operand2;
+			case '-':
+				return operand1 - operand2;
+			case '*':
+				return operand1 * operand2;
+			case '/':
+				return operand1 / operand2;
+			default:
+				return 0;
 		}
-		else if (token == '*') {
-			return multiply(pop(s, false));
-		}
-		else if (token == '/') {
-			return divide(pop(s, false));
-		}
-		return Double.parseDouble(String.valueOf(token));
-	}
-	
-	public static double add(String s) {
-		return opSwitch(s) + opSwitch(pop(s, true));
-	}
-	public static double subtract(String s) {
-		return opSwitch(s) - opSwitch(pop(s, true));
-	}
-	public static double multiply(String s) {
-		return opSwitch(s) * opSwitch(pop(s, true));
-	}
-	public static double divide(String s) {
-		return opSwitch(s) / opSwitch(pop(s, true));
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -54,7 +64,7 @@ public class PreFix {
 		while(sc.hasNextLine()) {
 			
 			String line = sc.nextLine();
-			System.out.println(line + " = " + opSwitch(line));
+			System.out.println(line + " = " + evaluate(line + " "));
 		}
 		sc.close();
 	}
